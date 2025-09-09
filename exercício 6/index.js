1db429ac88ca951d9da83bc1396967694d56089f
Ana Júlia Rubim <anajrbcosta25@gmail.com>
2025-09-09 19:32:18 -0300

    Create index.js

---
+++ exercício 6/index.js
@@ -0,0 +1,14 @@
+function pegarValor() {
+    let data = document.getElementById('data').value
+
+    let meses = [ "janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
+
+    let partes = data.split("/")
+
+    let dia = partes[0]
+    let mes = partes[1]
+    let ano = partes[2]
+
+    const paragrafo = document.getElementById('exibeValor');
+    paragrafo.textContent = `${dia} de ${meses[mes - 1]} de ${ano}`;
+}
